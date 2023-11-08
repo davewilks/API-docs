fc808a47159e1c754b5a120e9d3a72718b6a1d62
davewilksatkeyavi <david.wilks@keyavi.com>
2023-11-08 14:20:37 -0700

    Defenestrator
    Adding defenestrator stuff

---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+﻿define({r:0.5,st:'Stem',n:1,t:[0],m:[0],u:['../Content/API-docs/api-conceptual-docs.htm'],s:['+','limit'],p:[['+',40],['and',42],['click',27],['displayed',45],['full',31],['is',15],['not',14],['profile',16],['share',38],['they\u0027ll',2],['what\u0027s',28]]});
